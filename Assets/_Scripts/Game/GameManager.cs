fb6afcb0ea842be47870f95fb7e0d525098d238b
Gebruiker <ik_ben_ramses@hotmail.com>
2016-09-24 16:58:28 +0200

    Fixed positioning problem in menu and made the game spawn the 'current level'

---
+++ Assets/_Scripts/Game/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Ramses.Confactory;
+
+public class GameManager : MonoBehaviour {
+
+	private ConCurrentLevel conCurrentLevel;
+
+	protected void Awake ()
+	{
+		Initialize();
+	}
+
+	protected void Start()
+	{
+		StartGame();
+	}
+
+	private void Initialize()
+	{
+		conCurrentLevel = ConfactoryFinder.Instance.Give<ConCurrentLevel>();
+    }
+
+	private void StartGame()
+	{
+		conCurrentLevel.CreateCurrentLevelObject();
+    }
+}
